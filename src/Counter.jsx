58c10bb734c0eb25d9052d1867ac2d7a41677753
Shahid Bahadur <shahidbahadur1998@gmail.com>
2022-03-15 12:00:34 +0500

    input change

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,28 +1,43 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from './CounterReducer'
+// import { increment, decrement } from './CounterReducer'
+import { changetext, changevalue } from './CounterReducer'
 
 const Counter = () => {
-  const count = useSelector((state) => state.counter.count)
+  // const count = useSelector((state) => state.counter.count)
+  const text = useSelector((state) => state.counter.text)
+  const value = useSelector((state) => state.counter.value)
 
   const dispatch = useDispatch()
+
   return (
-    <div className='flex h-[90vh] justify-center items-center'>
-      <button
+    <div className='flex flex-col h-[90vh] justify-center items-center'>
+      {/* <button
         className='bg-sky-600 rounded text-white py-1 px-2'
         onClick={() => dispatch(increment())}
-      >
+        >
         Increment
-      </button>
-      <p className='mx-2'>{count}</p>
-      <button
-        className='bg-sky-600 rounded text-white py-1 px-2'
-        onClick={() => {
-          dispatch(decrement())
-        }}
-      >
-        Decrement
-      </button>
+        </button>{' '}
+      <p className='mx-2'>{count}</p> */}
+      <div className='flex'>
+        <input
+          type='text'
+          className='rounded outline-0 border border-slate-700 py-1 mr-2 indent-1'
+          value={text}
+          onChange={(e) => dispatch(changetext(e.target.value))}
+        />
+        <button
+          className='bg-sky-600 rounded text-white py-1 px-2'
+          onClick={() => {
+            dispatch(changevalue(text))
+          }}
+        >
+          Decrement
+        </button>
+      </div>
+      <div>
+        <p>{value}</p>
+      </div>
     </div>
   )
 }
--- src/CounterReducer.jsx
+++ src/CounterReducer.jsx
@@ -1,22 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: 'shahid',
   initialState: {
-    count: 0,
+    // count: 0,
+    text: '',
+    value: '',
   },
   reducers: {
-    increment: (state) => {
-      state.count += 1
+    changetext: (state, action) => {
+      state.text = action.payload
     },
-    decrement: (state) => {
-      if (state.count > 0) {
-        state.count -= 1
-      }
+    changevalue: (state, action) => {
+      state.value = action.payload
     },
+    // increment: (state) => {
+    //   state.count += 1
+    // },
+    // decrement: (state) => {
+    //   if (state.count > 0) {
+    //     state.count -= 1
+    //   }
+    // },
   },
 })
 
-export const { increment, decrement } = counterSlice.actions
+// export const { increment, decrement } = counterSlice.actions
+export const { changetext, changevalue } = counterSlice.actions
 
 export const counterReducer = counterSlice.reducer
